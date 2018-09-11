86eecf997c88a839938ce8978827ea1a7f8a7055
Curtis C <curtis.chat.hk@gmail.com>
2018-09-11 16:34:34 -0400

    Step 2: added button for AJAX request

--- ajax.js
+++ ajax.js
@@ -2,11 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
   //retrieves information at the root path of the server, by sending an empty JavaScript object, and expecting a test response.
   /* Your code goes here */
-  $.ajax({
-    url:'http://first-ajax-api.herokuapp.com/count',
-    method: 'GET',
-    data: {},
-    dataType: 'text'
+
+  var requestbtn= document.querySelector('button')
+
+  requestbtn.addEventListener('click', function() {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    })
   })
+
 
 });
